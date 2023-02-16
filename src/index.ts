af7e1e470debf19c71f7ca94e7cb3ccf3b5a2fed
RaulMullor <mullor92@gmail.com>
2023-02-16 20:24:19 +0100

    Create enviroment and install dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-const a = 4;
+import "./enviroments.js";
+import express from "express";
+import debug from "debug";
+
+const app = express();
+const logger = debug("cusetes:root");
+const port = process.env.PORT ?? 4000;
+
+app.get("/cusetes", (req, res) => {
+  logger("Hello World");
+});
+
+app.listen(port, () => {
+  logger(`Listen from port ${port}`);
+});
