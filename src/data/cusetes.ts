bdb7b840349b8a79abecf7266e9c360df77a1b91
RaulMullor <mullor92@gmail.com>
2023-02-16 21:18:09 +0100

    Create types for ThingsStructure

---
+++ src/data/cusetes.ts
@@ -0,0 +1,22 @@
+import { type ThingStructure } from "../types";
+
+const thingIknowDo: ThingStructure[] = [
+  {
+    id: 1,
+    things: "breath",
+  },
+  {
+    id: 2,
+    things: "eat",
+  },
+  {
+    id: 3,
+    things: "poo",
+  },
+  {
+    id: 4,
+    things: "pee",
+  },
+];
+
+export default thingIknowDo;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "./enviroments.js";
 import express from "express";
 import debug from "debug";
+import cusetes from "./data/cusetes.js";
 
 const app = express();
 const logger = debug("cusetes:root");
-const port = process.env.PORT ?? 4000;
+const port = process.env.PORT ?? 4001;
 
 app.get("/cusetes", (req, res) => {
   logger("Hello World");
+  res.status(200).json({ cusetes });
 });
 
 app.listen(port, () => {
